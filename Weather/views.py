67a4a39502ac9827c6a9a0462dab598fba83d78f
SimranNaik25 <simrannaik1600@gmail.com>
2024-06-05 15:58:43 +0530

    First commit

--- Weather/views.py
+++ Weather/views.py
@@ -1,18 +1,28 @@
-
+# weather/views.py
 
 from django.shortcuts import render
+import requests
 
-
-
-# Create your views here.
 def home(request):
-    return render(request,'Weather/home.html') 
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        weather_data = get_weather_data(city)
+        return render(request, 'weather/home.html', {'weather_data': weather_data})
+    else:
+        return render(request, 'weather/home.html')
 
-
-
-
-
-
-    
-        
-    
+def get_weather_data(city):
+    api_key = 'ee9f8cf325814bcd0dc92f3f85bc0ce7'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        weather_data = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+        }
+    else:
+        weather_data = {'error': 'City not found'}
+    return weather_data
--- weather_app/urls.py
+++ weather_app/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from Weather import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Weather.urls')),
+    path('',views.home,name='home'),
 
 ]
