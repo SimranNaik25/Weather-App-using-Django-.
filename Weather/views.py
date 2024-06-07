c9394179ac8d9ab43ef216af6a4548d99c11a268
SimranNaik25 <simrannaik141@gmail.com>
2024-06-07 14:36:32 +0530

    Update views.py

--- Weather/views.py
+++ Weather/views.py
@@ -1,15 +1,14 @@
-# weather/views.py
 
 from django.shortcuts import render
 import requests
+from datetime import datetime
 
 def home(request):
+    weather_data = None
     if request.method == 'POST':
         city = request.POST.get('city')
         weather_data = get_weather_data(city)
-        return render(request, 'weather/home.html', {'weather_data': weather_data})
-    else:
-        return render(request, 'weather/home.html')
+    return render(request, 'weather/home.html', {'weather_data': weather_data})
 
 def get_weather_data(city):
     api_key = 'ee9f8cf325814bcd0dc92f3f85bc0ce7'
@@ -22,8 +21,14 @@ def get_weather_data(city):
             'temperature': data['main']['temp'],
             'description': data['weather'][0]['description'],
             'icon': data['weather'][0]['icon'],
+            'humidity': data['main']['humidity'],
+            'pressure': data['main']['pressure'],
+            'wind_speed': data['wind']['speed'],
+            'sunrise': datetime.fromtimestamp(data['sys']['sunrise']).strftime('%H:%M:%S'),
+            'sunset': datetime.fromtimestamp(data['sys']['sunset']).strftime('%H:%M:%S'),
         }
     else:
         weather_data = {'error': 'City not found'}
     return weather_data
+
 
